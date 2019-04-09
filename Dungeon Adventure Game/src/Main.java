f90bbecad4d6b5ea5acb5c3e0eae92220c0f051d
Smartin9181 <smartin9181@gmail.com>
2019-04-09 11:35:08 -0400

    Started the code

---
+++ Dungeon Adventure Game/src/Main.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Objects included in the game
+        Scanner in = new Scanner(System.in);
+        Random rand = new Random();
+
+        //Var sets and data sets for enemies
+        String[] foe = { "Necromancer", "Sorcerer", "Wizard", "Evil Witch"};
+        int HPEnemy = 125;
+        int DamageDealtFromEnemy = 75;
+
+        //Var sets and data sets for player
+        int PlayerHP = 150;
+        int DamageDealtFromPlayer = 85;
+        int AmountOfHealingPotions = 3;
+        int HealthRestoration = 25;
+        int PotDropPercent = 50; //This value is a percentage
+
+        boolean activated = true;
+        System.out.println("Wizard's Quest!");
+
+        GAME:
+        while(activated) {
+            System.out.println("----------------------------------------------");
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
