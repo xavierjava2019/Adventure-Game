85fa7ca4e2ccb7d4bb855cfe83cd6f893a7d7f57
Smartin9181 <smartin9181@gmail.com>
2019-04-10 11:35:06 -0400

    Code now displays enemies, their health and your health, and options for battle

--- Dungeon Adventure Game/src/Main.java
+++ Dungeon Adventure Game/src/Main.java
@@ -10,7 +10,7 @@ public class Main {
 
         //Var sets and data sets for enemies
         String[] foe = { "Necromancer", "Sorcerer", "Wizard", "Evil Witch"};
-        int HPEnemy = 125;
+        int HPEnemyMax = 125;
         int DamageDealtFromEnemy = 75;
 
         //Var sets and data sets for player
@@ -21,11 +21,30 @@ public class Main {
         int PotDropPercent = 50; //This value is a percentage
 
         boolean activated = true;
-        System.out.println("Wizard's Quest!");
+        System.out.println("Magic Quest!");
 
         GAME:
         while(activated) {
             System.out.println("----------------------------------------------");
+
+            int HPEnemy = rand.nextInt(HPEnemyMax);
+            String foes = foe[rand.nextInt(foe.length)];
+            System.out.println("\t# " + foes + " has arrived! #\n");
+
+            while(HPEnemy > 0) {
+                System.out.println("\tYour Health Points: " + PlayerHP);
+                System.out.println("\t" + foes + "'s Health Points: " + HPEnemy);
+                System.out.println("\n\tChoose an action");
+                System.out.println("\t1. Strike foe");
+                System.out.println("\t2. Consume Healing Potion");
+                System.out.println("\t3. Flee");
+
+            }
+
+
+
+
+
 
 
 
